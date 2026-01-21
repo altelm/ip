8a9fe86d7425647b453d0b99bd77d0d58f55a05c
Raghad <e1375440@u.nus.edu>
2026-01-21 14:37:44 +0800

    Chatbot now echoes commands

--- src/main/java/Esm.java
+++ src/main/java/Esm.java
@@ -1,7 +1,15 @@
+import java.util.*;
+import java.io.*;
 public class Esm {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Chat\nWhat can I do for you?\n");
+        String in = scanner.nextLine();
+        while(!in.equalsIgnoreCase("bye")) {
+            System.out.println(in);
+            in = scanner.nextLine();
+        }
         System.out.println("Bye. Hope to see you again\n");
 
     }
