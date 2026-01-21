490d17cf87c19799bcb7f53745242ce4ed683a88
Raghad <e1375440@u.nus.edu>
2026-01-21 14:11:16 +0800

    Rename file and implement basics

---
+++ src/main/java/Esm.java
@@ -0,0 +1,8 @@
+public class Esm {
+    public static void main(String[] args) {
+
+        System.out.println("Hello! I'm Chat\nWhat can I do for you?\n");
+        System.out.println("Bye. Hope to see you again\n");
+
+    }
+}
