9b12f359751bd851c4910fe38336e00556112032
Raghad <e1375440@u.nus.edu>
2026-01-21 14:53:43 +0800

    Ability to store and display text

--- src/main/java/Esm.java
+++ src/main/java/Esm.java
@@ -4,10 +4,19 @@ public class Esm {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello! I'm Chat\nWhat can I do for you?\n");
+        ArrayList<String> list= new ArrayList<>();
+        System.out.println("Hello! I'm Esm\nWhat can I do for you?\n");
         String in = scanner.nextLine();
         while(!in.equalsIgnoreCase("bye")) {
-            System.out.println(in);
+            if(in.equalsIgnoreCase("list")) {
+                for(int i = 1; i <= list.size(); i++) {
+                    System.out.println( i + ". " + list.get(i-1));
+                }
+            } else {
+                list.add(in);
+                System.out.println("added: "+ in);
+            }
+
             in = scanner.nextLine();
         }
         System.out.println("Bye. Hope to see you again\n");
