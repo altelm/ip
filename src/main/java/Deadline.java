bae9bcd3c1407aba5af1b061096f9c9c9b4629dd
Raghad <e1375440@u.nus.edu>
2026-02-04 20:53:38 +0800

    Implement Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,25 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
 
-    private String deadline;
+    private LocalDate deadline;
+    private static final DateTimeFormatter PRI = DateTimeFormatter.ofPattern("MMM dd yyyy");
+
     public Deadline(String[] nameAndDeadline) {
         super("D", nameAndDeadline[0]);
-        this.deadline = nameAndDeadline[1].substring(3);
+        System.out.println(nameAndDeadline[1]);
+        try {
+            this.deadline = LocalDate.parse(nameAndDeadline[1].trim().split("\\s+")[1]);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("Holdeth and useth yyyy-mm-dd");
+        }
     }
+
 
     @Override
     public String toString() {
-        return super.toString() + "(by: " + this.deadline + ")";
+        return super.toString() + "(by: " + this.deadline.format(PRI) + ")";
     }
 }
